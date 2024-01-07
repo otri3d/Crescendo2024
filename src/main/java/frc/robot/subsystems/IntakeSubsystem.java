de5a7ff37d1b5b01b731e53bb7b6cde70620c8a9
Quentin Dela Torre <quentin.delatorre@ontariotechu.net>
2024-01-07 17:12:16 -0500

    add more Intake methods

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -1,3 +1,4 @@
+
 // Copyright (c) FIRST and other WPILib contributors.
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
@@ -10,25 +11,21 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.Encoder;
-import edu.wpi.first.wpilibj.CounterBase.EncodingType;
 import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 import frc.robot.Constants.OperatorConstants;
-import frc.robot.Constants.InputConstants;;
-
+import frc.robot.Constants.InputConstants;
 public class IntakeSubsystem extends SubsystemBase {
 
     private WPI_VictorSPX m_intakearm1, m_intakearm2, m_gripper;
-    private MotorControllerGroup m_intake;
+    private MotorControllerGroup m_intakearms = new MotorControllerGroup(m_intakearm1,m_intakearm2);
     private DigitalInput upper_limit, lower_limit;
-    private Encoder encoder;
+    private Encoder encoder = new Encoder(InputConstants.ENCODERDIO1, InputConstants.ENCODERDIO2, false, Encoder.EncodingType.k2X);
 
   /** Creates a new ExampleSubsystem. */
     public IntakeSubsystem(){
-      m_intakearm1 = new WPI_VictorSPX(OperatorConstants.MOTORCONTROLPORT5);
-      m_intakearm2 = new WPI_VictorSPX(OperatorConstants.MOTORCONTROLPORT6);
-      m_gripper = new WPI_VictorSPX(OperatorConstants.MOTORCONTROLPORT7);
-      m_intake = new MotorControllerGroup(m_intakearm1, m_intakearm2);
-      encoder = new Encoder(InputConstants.ENCODERDIO1, InputConstants.ENCODERDIO2, false, EncodingType.k1X);
+        m_intakearm1 = new WPI_VictorSPX(OperatorConstants.MOTORCONTROLPORT5);
+        m_intakearm2 = new WPI_VictorSPX(OperatorConstants.MOTORCONTROLPORT6);
+        m_gripper = new WPI_VictorSPX(OperatorConstants.MOTORCONTROLPORT7);
     }
 
   /**
@@ -44,19 +41,21 @@ public class IntakeSubsystem extends SubsystemBase {
           /* one-time action goes here */
         });
   }
+  public void setIntakeArmVelocity(double controllerInput){
+    m_intakearms.set(controllerInput);
+    if(encoder.get() == 0){
 
-public void setIntakePosition(double position){
-  if(encoder.getDistance() == position){
-    m_intake.set(0);
-  } 
-  else if(encoder.getDistance() < position){
-    m_intake.set(1);
-  } 
-  else if(encoder.getDistance() > position){
-    m_intake.set(-1);
+    }
   }
-}
 
+  public void toggleGripperVelocty(){
+    if(encoder.get() != 0){
+        m_gripper.set(0);
+    }
+    if(encoder.get() == 0){
+        m_gripper.set(1);
+    }    
+  }
   /**
    * An example method querying a boolean state of the subsystem (for example, a digital sensor).
    *
