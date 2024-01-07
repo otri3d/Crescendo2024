0f194dd6eb6921d4a4b73488e15fc4766d86740e
Kershan Arulneswaran <74938655+Koops0@users.noreply.github.com>
2024-01-07 15:20:20 -0500

    Added some basic code for Elevator and Intake Subsystems. Drivetrain still needs to be finished

---
+++ src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
@@ -0,0 +1,49 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
+import edu.wpi.first.wpilibj.DigitalInput;
+
+import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
+import frc.robot.Constants.OperatorConstants;
+
+public class ElevatorSubsystem extends SubsystemBase {
+    private WPI_VictorSPX m_elevator1, m_elevator2;
+    private MotorControllerGroup m_elevator;
+    private DigitalInput upper_lim, lower_lim;
+
+    public ElevatorSubsystem(){
+
+    }
+
+     public Command exampleMethodCommand() {
+    // Inline construction of command goes here.
+    // Subsystem::RunOnce implicitly requires `this` subsystem.
+    return runOnce(
+        () -> {
+          /* one-time action goes here */
+        });
+  }
+
+  /**
+   * An example method querying a boolean state of the subsystem (for example, a digital sensor).
+   *
+   * @return value of some boolean subsystem state, such as a digital sensor.
+   */
+  public boolean exampleCondition() {
+    // Query some boolean state, such as a digital sensor.
+    return false;
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+
+  @Override
+  public void simulationPeriodic() {
+    // This method will be called once per scheduler run during simulation
+  }
+}
